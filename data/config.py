f370807b26ca8f7cef5ef6e4e53efe9a352be9cf
aqsomath <aqsomath@gmail.com>
2023-11-08 21:53:52 +0500

    first commit

--- data/config.py
+++ data/config.py
@@ -1,13 +1,22 @@
-from environs import Env
+# from environs import Env
+#
+# # environs kutubxonasidan foydalanish
+# env = Env()
+# env.read_env()
+# # .env fayl ichidan quyidagilarni o'qiymiz
+# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+# DB_USER=env.str("DB_USER")
+# DB_PASS=env.str("DB_PASS")
+# DB_HOST=env.str("DB_HOST")
+# DB_NAME=env.str("DB_NAME")
 
-# environs kutubxonasidan foydalanish
-env = Env()
-env.read_env()
+
+import os
+
 # .env fayl ichidan quyidagilarni o'qiymiz
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-DB_USER=env.str("DB_USER")
-DB_PASS=env.str("DB_PASS")
-DB_HOST=env.str("DB_HOST")
-DB_NAME=env.str("DB_NAME")
-
-
+BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))  # Bot token
+ADMINS = list(os.environ.get("ADMINS"))  # adminlar ro'yxati
+DB_USER = list(os.environ.get("DB_USER"))
+DB_HOST = list(os.environ.get("DB_HOST"))
+DB_NAME = list(os.environ.get("DB_NAME"))
+DB_PASS = list(os.environ.get("DB_PASS"))
