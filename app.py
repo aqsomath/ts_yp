2bbffb575a2d79196e59c337796afacaadab1a58
aqsomath <aqsomath@gmail.com>
2023-11-10 10:18:04 +0500

    add deploy

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ from utils.set_bot_commands import set_default_commands
 
 async def on_startup(dispatcher):
     # Birlamchi komandalar (/star va /help)
-    # await db.create()
-    # await db.drop_orders()
-    # await db.create_table_users()
-    # await db.create_table_orders()
+    await db.create()
+    await db.drop_orders()
+    await db.create_table_users()
+    await db.create_table_orders()
     await set_default_commands(dispatcher)
 
     # Bot ishga tushgani haqida adminga xabar berish
